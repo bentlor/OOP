5e0f18706383045f2804d180a1ae95ab387ee444
Bence Szabo <Bence_Szabo@epam.com>
2016-08-09 13:06:12 +0200

    oop first commit

---
+++ src/com/epam/bence_szabo/oop/Itinerary.java
@@ -0,0 +1,5 @@
+package com.epam.bence_szabo.oop;
+
+public class Itinerary {
+
+}
---
+++ src/com/epam/bence_szabo/oop/OccasionalFlyer.java
@@ -0,0 +1,5 @@
+package com.epam.bence_szabo.oop;
+
+public class OccasionalFlyer implements FrequentFlyerProgram {
+
+}
